bf73f0ebed268da6ead89219264ff7957721d6de
Giovanni Pires da Silva <giovannipds@gmail.com>
2019-10-24 11:58:03 -0300

    Avoid getAccessToken error if running on server and localStorage not available

--- services/auth.js
+++ services/auth.js
@@ -1,6 +1,11 @@
 export const ACCESS_TOKEN_KEY = 'access_token'
 
-export const getAccessToken = _ => localStorage.getItem(ACCESS_TOKEN_KEY)
+export const getAccessToken = _ => {
+  if (typeof window === 'undefined') {
+    return null
+  }
+  return localStorage.getItem(ACCESS_TOKEN_KEY)
+}
 
 export const setAccessToken = accessToken => {
   localStorage.setItem(ACCESS_TOKEN_KEY, accessToken)
