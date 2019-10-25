808f1a8d6b912e4c38930d2911a6465949e84b9d
Giovanni Pires da Silva <giovannipds@gmail.com>
2019-10-25 14:40:49 -0300

    Fix is_production import at api service

--- services/api.js
+++ services/api.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import { getAccessToken } from './auth'
+import { IS_PRODUCTION } from '../constants/constants'
 
 export const baseURL = IS_PRODUCTION ? 'http://127.0.0.1:8000' : 'http://dale.sa-east-1.elasticbeanstalk.com'
 
