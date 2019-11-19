15841b529bf3864550b3e095732c23a4ded93379
Giovanni Pires da Silva <giovanni@somosgad.com>
2019-11-19 15:28:15 -0300

    Fix production api url

--- constants/constants.js
+++ constants/constants.js
@@ -16,4 +16,7 @@ export const IS_PRODUCTION = process.env.NODE_ENV === 'production'
 export const ONLINE = IS_PRODUCTION
 export const CLIENT_SECRET = 'IxY0z1pn27XwHNeFD5mj34ok34uqbEa5cehhQza4teste'
 export const CLIENT_ID = 2
-export const API_URL = IS_PRODUCTION ? 'http://dale.sa-east-1.elasticbeanstalk.com' : process.env.API_URL || 'http://127.0.0.1:8000'
+
+const LOCAL_API_URL = process.env.API_URL || 'http://127.0.0.1:8000'
+
+export const API_URL = IS_PRODUCTION ? 'https://admin.dalecampeon.net' : LOCAL_API_URL
