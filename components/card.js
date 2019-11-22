13f970a7af4389f0f4031be3964dedffd0b5746d
Giovanni Pires da Silva <giovanni@somosgad.com>
2019-11-22 11:32:45 -0300

    Show placeholder if card thumbnail doesnt exist

--- components/card.js
+++ components/card.js
@@ -1,10 +1,11 @@
 import MediaLink from './MediaLink/MediaLink'
 
 const Card = ({category, media}) => {
+  const thumbnail_url = media.thumbnail_url || 'http://placehold.jp/180x255.png'
   return (
     <MediaLink {...{category, media}}>
       <a className="d-block carousel-card">
-        <img className="img-fluid" height="256" src={media.thumbnail_url} width="180" />
+        <img className="img-fluid" height="256" src={thumbnail_url} width="180" />
         <style jsx>{`
           .carousel-card {
             display: block;
