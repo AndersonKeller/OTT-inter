15e838bf54151d7a1063d26ed9a557cbd9bde063
Giovanni Pires da Silva <giovanni@somosgad.com>
2019-12-17 18:08:42 -0300

    Gallardo cover for desktop

--- pages/index.js
+++ pages/index.js
@@ -12,14 +12,14 @@ import CarouselSection from '../components/carousel-section'
 import Featured from '../components/featured'
 import Layout from '../components/layout/Layout'
 import Loading from '../components/Loading/Loading'
+import MediaLink from '../components/MediaLink/MediaLink'
 import WishlistBtn from '../components/wishlist-btn'
 import UserContext from '../components/UserContext'
-import { STATIC_PATH, TENANT } from '../constants/constants'
 import { CONFIG } from '../config'
 import api from '../services/api'
 
 // home page
-const Home = ({ layoutProps }) => {
+const Home = ({ featuredMedia, featuredMediaError, layoutProps }) => {
   const { user } = useContext(UserContext)
   return (
     <Layout {...layoutProps} paddingTop={false}>
@@ -29,7 +29,7 @@ const Home = ({ layoutProps }) => {
       <div className="index">
 
         {/* cover */}
-        <Cover />
+        <Cover media={featuredMedia} />
 
         {/* supporters */}
         <HomeCarouselSection category="supporters" />
@@ -72,96 +72,93 @@ const Home = ({ layoutProps }) => {
   )
 }
 
+Home.getInitialProps = async _ => {
+  try {
+    const { data: { movie: featuredMedia } } = await api.get('movie/marcelo-gallardo-lo-jugamos-como-una-final?for=home-cover')
+    return { featuredMedia }
+  } catch (error) {
+    return { error: featuredMediaError }
+  }
+}
+
 export default Home
 
 // cover
-const Cover = _ => {
+const Cover = ({ media }) => {
   const { user } = useContext(UserContext)
   return (
     <div className="cover">
       <div className="container-fluid">
         <div className="row">
-          <div className="col col-8 col-md-5">
-            <div className="description">
-              <h1 className="h1">
-                <span className="h1-a">Entrevista a nuestro</span>
-                <strong className="h1-b">{
-                  TENANT === 'dalecampeon' ? `“Napoléon”` : `Comandante`
-                }</strong>
-                <span className="h1-c">{
-                  TENANT === 'dalecampeon' ? 'Marcelo Gallardo' : 'Mario Salas'
-                }</span></h1>
-              { ! user ? (
-                <>
-                  <Link href="/subscriptor" passHref>
-                    <Button>Probar Gratis</Button>
-                  </Link>
-                  <Link href="/movies" passHref>
-                    <Button color="secondary" outline>Ver más</Button>
-                  </Link>
-                </>
-              ) : (
-                <>
-                  <Link href="/movies" passHref>
-                    <Button>Ver más</Button>
-                  </Link>
-                  <WishlistBtn movieId={17} />
-                </>
-              )}
+          <div className="col col-8 col-md-4 offset-md-1">
+            <div className="cover__interaction">
+
+              <div className="cover__infos">
+                <h1 className="cover__logo">
+                  <img className="img-fluid" height={media.logo.height} src={media.logo.url} width={media.logo.width} />
+                </h1>
+                {media.description && (
+                  <div className="cover__description">
+                    <p>{media.description}</p>
+                  </div>
+                )}
               </div>
+
+              { ! user ? (
+                <Link href="/subscriptor" passHref>
+                  <Button>Probar Gratis</Button>
+                </Link>
+              ) : (
+                <div>
+                  <MediaLink {...{media}} passHref>
+                    <Button>Mira</Button>
+                  </MediaLink>
+                  <WishlistBtn movieId={media.id} />
+                </div>
+              )}
+
+            </div>
           </div>
         </div>
       </div>
       <style jsx>{`
         .cover {
           align-items: center;
-          background-image: url(${STATIC_PATH}/cover.png);
-          background-position: 50% 50%;
-          background-repeat: no-repeat;
-          background-size: cover;
+          background-image:
+            linear-gradient(to bottom, rgba(0,0,0,0) 80%, black 100%),
+            radial-gradient(circle at 67.5% 57.5%, rgba(0,0,0,0) 25%, rgba(0,0,0,.925) 42.5%),
+            url(${media.poster_url});
+          background-position: 50% 50%, 50% 50%;
+          background-repeat: no-repeat, no-repeat;
+          background-size: cover, cover;
           display: flex;
-          height: 640px;
-          margin-bottom: 20px;
-          min-height: 90vh;
+          height: 650px;
+          margin-bottom: -60px;
+          min-height: 100vh;
           overflow: hidden;
           padding-top: var(--padding-top);
-          padding-left: 9%;
+          padding-bottom: 60px;
+        }
+        .cover__infos {
+          margin-bottom: 30px;
+        }
+        .cover__logo {
+          margin-top: 0;
+          margin-bottom: 15px;
+          position: relative;
+        }
+        .cover__logo::before {
+          content: '';
+          display: block;
+          padding-bottom: ${media.logo.height * 100 / media.logo.width + '%'};
+        }
+        .cover__logo img {
+          left: 0;
+          position: absolute;
+          top: 0;
         }
         .cover :global(.btn) {
           margin-right: 15px;
-        }
-        .h1 {
-          display: flex;
-          flex-direction: column;
-          font-family: 'Bebas Neue Book';
-          font-size: 31px;
-          font-weight: normal;
-          line-height: .98;
-          margin-top: 0;
-          margin-bottom: 20px;
-          text-transform: uppercase;
-        }
-        @media (min-width: 768px) {
-          .h1 {
-            font-size: 62px;
-          }
-        }
-        .h1-a {
-          position: relative;
-          z-index: 2;
-        }
-        .h1-b {
-          color: var(--primary);
-          font-family: 'Bebas Neue';
-          font-size: 2.24em; /* 139px */
-          font-weight: bold;
-          margin-left: -50px;
-          position: relative;
-          z-index: 1;
-        }
-        .h1-c {
-          font-size: 1.16em;
-          margin-top: -.20em; /* -15px */
         }
       `}</style>
     </div>
@@ -248,11 +245,11 @@ const BannerSection = ({bannerID: id, movieID}) => {
     }
     fetchData()
   }, [id])
-  
+
   return (
     <div>
       <div className="section">
-        
+
         {/* loading */}
         <div className="text-center">
           <Loading loadingState={loading} />
@@ -277,13 +274,13 @@ const BannerSection = ({bannerID: id, movieID}) => {
               </Featured>
             )}
             </div>
-        )} 
+        )}
 
         {/* error */}
         {error && (
           <div className="text-center">Error</div>
         )}
-        
+
       </div>
       <style jsx>{`
         .sponsor-link {
