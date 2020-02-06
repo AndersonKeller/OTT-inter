0981135491524b0e57b87f21c2512abd429436be
Giovanni Pires da Silva <giovanni@somosgad.com>
2020-02-06 19:20:30 -0300

    Update min plan price at subscriptor

--- pages/subscriptor.js
+++ pages/subscriptor.js
@@ -420,7 +420,7 @@ const Section1 = () => {
             </div>
             <H2 className="text-uppercase section1__title">¡Bienvenidos!</H2>
             <p>{CONFIG.fullClubName} te da la bienvenida a la plataforma de contenidos del Más Grande!</p>
-            <p>Todo por <big>99</big> pesos mensuales.</p>
+            <p>Todo por <big>149</big> pesos mensuales.</p>
           </div>
         </div>
       </div>
