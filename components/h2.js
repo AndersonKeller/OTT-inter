ebc438c8b2afba08fdf1503d9da61230aaeac002
Giovanni Pires da Silva <giovanni@somosgad.com>
2020-02-13 11:00:07 -0300

    Fix styled components h2 warning

--- components/h2.js
+++ components/h2.js
@@ -1,26 +1,27 @@
-import styled, { ThemeProvider } from 'styled-components'
+import styled from 'styled-components'
 import { spacing } from '@material-ui/system'
 
-const H2 = ({ children, uppercase, theme, ...other }) => {
+const StyledH2 = styled.h2`
+  ${spacing}
+  text-transform: ${props => props.uppercase ? 'uppercase': 'none'};
+  @media (min-width: 768px) {
+    --font-size: 22px;
+  }
+  @media (min-width: 992px) {
+    --font-size: 31px;
+  }
+`
 
+const H2 = ({ children, uppercase, theme, ...other }) => {
   const extendTheme = {
     ...theme,
     spacing: value => value * 5,
   }
-
-  const StyledH2 = styled.h2`${spacing}
-    text-transform: ${uppercase ? 'uppercase': 'none'};
-    @media (min-width: 768px) {
-      --font-size: 22px;
-    }
-    @media (min-width: 992px) {
-      --font-size: 31px;
-    }
-  `
-
-  return <>
-    <StyledH2 theme={extendTheme} {...other}>{children}</StyledH2>
-  </>
+  return (
+    <StyledH2 theme={extendTheme} uppercase={uppercase} {...other}>
+      {children}
+    </StyledH2>
+  )
 }
 
 export default H2
