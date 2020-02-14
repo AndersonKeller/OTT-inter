8e957f9f94182a47e542b1e39eb0c537f7557109
Giovanni Pires da Silva <giovanni@somosgad.com>
2020-02-14 13:07:45 -0300

    Revert 24f620a (not working in production)

--- constants/constants.js
+++ constants/constants.js
@@ -32,7 +32,7 @@ export const CLIENT_ID = 2
 
 const LOCAL_API_URL = process.env.API_URL || 'http://127.0.0.1:8000'
 
-export const API_URL = IS_PRODUCTION ? 'https://admin.rivermas.com' : LOCAL_API_URL
+export const API_URL = IS_PRODUCTION ? 'https://admin.dalecampeon.net' : LOCAL_API_URL
 
 /**
  * @constant
