818f02326ad64b7ede5042a0066de6745c9be8db
Giovanni Pires da Silva <giovanni@somosgad.com>
2020-03-13 14:54:13 -0300

    Add info

--- config/index.js
+++ config/index.js
@@ -12,7 +12,10 @@ import tenantsConfigs from './config.json'
  * @property {string} fullClubName - the full club's name / registered etc.
  * @property {string} shortClubName - the club's nickname or short name.
  * @property {string} supportersAKA - how the supporters are called.
- * @property {string} color - the clubs primary color.
+ * @property {string} color - the club's primary color.
+ * @property {string} site - the club's website.
+ * @property {array} socialNetworks - club's social networks.
+ * @property {string} googleAnalytics - analytics tracking code.
  */
 /** @type {AppsConfig} */
 export const CONFIG = tenantsConfigs[TENANT]
