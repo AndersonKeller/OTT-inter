0f42bd50ff1bc7292840e7a04946cfc3b62f5e1c
Giovanni Pires da Silva <giovanni@somosgad.com>
2020-04-19 13:56:34 -0300

    Check primary color and revert it back to darken instead of lighten

--- theme.js
+++ theme.js
@@ -4,8 +4,8 @@ import { CONFIG } from '~/config'
 
 const theme = {
   colors: {
-    primary: Color(CONFIG.color).lighten(.2).string(),
-    primaryHover: CONFIG.color,
+    primary: CONFIG.color,
+    primaryHover: Color(CONFIG.color).darken(.2).string(),
     primaryAlpha: Color(CONFIG.color).lighten(.2).alpha(.3).string(),
   },
 }
