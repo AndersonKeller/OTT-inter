3da8e56d3c8ada5a6f711a4924972ffaf56e8b84
DannerTerra <breakearth@gmail.com>
2020-04-29 21:17:43 -0300

    Fix verifyEmail page

--- pages/user/verifyEmail.js
+++ pages/user/verifyEmail.js
@@ -4,66 +4,32 @@ import Router, { useRouter } from 'next/router'
 
 // react imports
 import { useEffect, useState, useContext } from 'react'
+import nookies from 'nookies'
 
 // app imports
 import Layout from '../../components/layout/Layout'
 import { CONFIG } from '../../config'
-import api from '../../services/api'
-import Loading from '../../components/Loading/Loading'
-import { AuthModalContext } from '../../contexts/AuthModalContext'
+import withApi from '~/components/withApi'
+import Loading from '~/components/Loading/Loading'
 
-const CheckEmail = ({layoutProps, userMenuProps}) => {
+const CheckEmail = ({ layoutProps, message }) => {
 
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(false);
-  const { closeAuthModal, openAuthModal, setTab } = useContext(AuthModalContext)
-  const router = useRouter();
-  const verify = router.query.apiurl;
-
-  const fetchData = async _ => {
-    console.table(userMenuProps);
-    try {
-      const { status } = await api().get('',{baseURL: decodeURI(verify)})
-
-      if(status == 200 ){
-        setLoading(false)
-        setTimeout(() => {
-          openAuthModal('login')
-        }, 2000);
-      }else{
-        setLoading(false)
-        setError('Email could not be verified')
-        setTimeout(() => {
-          Router.push('/')
-        }, 2000);
-      }
-    } catch (e) {
-      setLoading(false)
-      setError('Link Expired')
-      setTimeout(() => {
-        Router.push('/')
-      }, 2000);
-    }
-  }
-
-  useEffect(() => {
-    fetchData();
-    // return () => {
-    //   cleanup
-    // };
+  useEffect(_ => {
+      message = JSON.stringify(message)
+      nookies.set({}, 'flash_message', message, { path: "/" })
+      Router.push("/")
   }, [])
 
   return (
     <Layout {...layoutProps}>
       <Head>
-        <title>Email Verification &lt; {CONFIG.appName}</title>
+        <title>Verificación de email &lt; {CONFIG.appName}</title>
       </Head>
       <div className="container-fluid">
         <div className="row">
           <div className="col-md-10 offset-md-1">
-            <h1 className="h2">Verificacion de email</h1>
-            <Loading loadingState={loading} />
-            {<h1 className="h6">Problem: </h1> && error}
+            <h1 className="h2">Verificación de email</h1>
+            <Loading loadingState={true} />
           </div>
         </div>
       </div>
@@ -84,4 +50,24 @@ const CheckEmail = ({layoutProps, userMenuProps}) => {
   );
 }
 
-export default CheckEmail
+CheckEmail.getInitialProps = async ctx => {
+  try {
+    const { api, query } = ctx
+    const verifyURL = query.apiurl
+    var message = {}
+    var path = "/"
+
+    const { status } = await api.get("",{baseURL: decodeURI(verifyURL)})
+    message = (status == 200) ?
+      {success: "Email verificado con éxito"}
+    :
+      {error: "Este correo electrónico no pudo ser verificado"}
+
+  } catch (e) {
+    message = {info: "Este enlace ha expirado"}
+  }
+
+  return { message }
+}
+
+export default withApi(CheckEmail)
