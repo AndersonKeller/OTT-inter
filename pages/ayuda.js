c29ee3b792013124af020574f180fb824ddf7415
Giovanni Pires da Silva <giovanni@somosgad.com>
2020-05-04 14:45:16 -0300

    Help page padding bottom + minor changes

--- pages/ayuda.js
+++ pages/ayuda.js
@@ -3,37 +3,40 @@ import Head from 'next/head'
 import Layout from '~/components/layout/Layout'
 import { CONFIG } from '~/config'
 import { WHITE } from '~/constants/colors'
-import api from '~/services/api'
 import { Accordion, Card, Button } from 'react-bootstrap'
+import withApi from '~/components/withApi'
 
-const Help = ({ layoutProps, title, faqs, ...props }) => {
-    return (
-    <Layout layoutColor="white" { ...layoutProps }>
-        <Head>
-           <title>{ title } &lt; {CONFIG.appName} </title>
-        </Head>
-        <div className="faqs-wrapper">
-            <div className="container">
-                <h3 className="faqs-title">Preguntas Frecuentes</h3>
-                <Accordion>
-                { faqs.map((item, key) => (
-                       <Card className="faqs-card">
-                          <Accordion.Toggle as={Card.Header} className="faqs-header" eventKey={key}>
-                                <h5 className="faqs-question">{ item.question }</h5>
-                          </Accordion.Toggle>
-                          <Accordion.Collapse eventKey={key}>
-                              <Card.Body>
-                                <p className="faqs-text">{ item.answer }</p>
-                              </Card.Body>
-                          </Accordion.Collapse>
-                       </Card>
-                )) }
-                </Accordion>
-            </div>
+const HelpPage = ({ layoutProps, title, faqs }) => {
+  return (
+    <Layout color="white" {...layoutProps}>
+      <Head>
+        <title>{title} &lt; {CONFIG.appName}</title>
+      </Head>
+      <div className="faqs-wrapper">
+        <div className="container">
+          <h3 className="faqs-title">Preguntas Frecuentes</h3>
+          <Accordion>
+            { faqs.map((item, key) => (
+              <Card className="faqs-card">
+                <Accordion.Toggle as={Card.Header} className="faqs-header" eventKey={key}>
+                  <h5 className="faqs-question">{ item.question }</h5>
+                </Accordion.Toggle>
+                <Accordion.Collapse eventKey={key}>
+                  <Card.Body>
+                    <p className="faqs-text">{ item.answer }</p>
+                  </Card.Body>
+                </Accordion.Collapse>
+              </Card>
+            )) }
+          </Accordion>
         </div>
-       <style global jsx>{`
+      </div>
+      <style global jsx>{`
+        .faqs-wrapper {
+          padding-bottom: 120px;
+        }
         .faqs-question{
-          margin:0;
+          margin: 0;
         }
         .faqs-title {
           font-size: 32px;
@@ -46,34 +49,31 @@ const Help = ({ layoutProps, title, faqs, ...props }) => {
           border-radius: 3px;
         }
         .card-body {
-          padding:5px 20px;
+          padding: 5px 20px;
         }
         .faqs-header {
           background-color: ${Color(WHITE).darken(.025).string()};
+          cursor: pointer;
           transition: background-color .2s;
         }
         .faqs-header:hover {
           background-color: ${Color(WHITE).darken(.1).string()};
         }
-        .faqs-wrapper {
-          background-color: var(--white);
-          color: var(--black);
-          min-height: 500px;
-        }
         .faqs-text {
           font-size: 18px;
           white-space: pre-wrap;
         }
-      '`}</style>
+      `}</style>
     </Layout>
-)}
-
-Help.getInitialProps = async ctx => {
-    const response = await api(ctx).get('/help');
-    const data = await response.data;
-    const {faqs} = data
-    const title = 'Ayuda';
-    return { faqs: faqs, ...{title}  };
+  )
 }
 
-export default Help;
+HelpPage.getInitialProps = async ctx => {
+  const { api } = ctx
+  const { data } = await api.get('help')
+  const { faqs } = data
+  const title = 'Ayuda'
+  return { faqs, title }
+}
+
+export default withApi(HelpPage)
