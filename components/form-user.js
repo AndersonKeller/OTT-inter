9252a5f30308da33e9980f13fc589d1e6ff68410
DannerTerra <breakearth@gmail.com>
2020-05-06 12:59:12 -0300

    Refactor userData page & extract form to form-user component

---
+++ components/form-user.js
@@ -0,0 +1,121 @@
+// react
+import { useEffect, useState }  from 'react'
+import { Formik, Form }         from 'formik'
+import * as Yup                 from 'yup'
+
+// components
+import { FkSelect, FkInput }  from '~/components/Formik/fields'
+import FormGroup              from '~/components/layout/AuthModal/FormGroup'
+import Button                 from '~/components/button'
+import { CONFIG }             from '~/config'
+import api                    from '~/services/api'
+
+
+
+const UserForm = ({handleSubmit, initialValues, button }) => {
+  const [ genders, setGenders ] = useState([])
+  const [ countries, setCountries ] = useState([])
+
+  /* get genders */
+  useEffect(_ => {
+    (async _ => {
+      const {data} = await api().get('genders')
+      setGenders(data)
+    })()
+  }, [])
+
+  /* get countries */
+  useEffect(_ => {
+    (async _ => {
+      const {data} = await api().get('countries')
+      setCountries(data)
+    })()
+  }, [])
+
+  return(
+    <Formik
+      initialValues={initialValues}
+      validationSchema={
+        yupSchema(countries, genders)
+      }
+      onSubmit={handleSubmit}
+      component={props => <DataForm {...{countries, genders, button, ...props}} />}
+    />
+  )
+}
+
+const DataForm = ({isSubmitting, countries, genders, button})  => {
+
+  const cityLabel = CONFIG.lang === 'es-CL' ? 'Provincia' : 'Ciudad'
+
+  return (
+    <Form>
+      <div className="row">
+        <div className="col-md-6">
+          <div className="data">
+            <h4>Tus datos</h4>
+            <FormGroup>
+              <FkInput name="name" label="Nombre Completo" />
+            </FormGroup>
+            <FormGroup>
+              <FkSelect name="gender" label="Género" list={genders} />
+            </FormGroup>
+            <FormGroup>
+              <FkInput name="document" label="Documento" />
+            </FormGroup>
+          </div>
+        </div>
+
+        <div className="col-md-6">
+          <div className="localization">
+            <h3 className="h3">Ubicación</h3>
+            <FormGroup>
+              <FkInput name="address" label="Dirección" />
+            </FormGroup>
+            <FormGroup>
+              <FkInput name="city" label={cityLabel} />
+            </FormGroup>
+            <FormGroup>
+              <FkSelect name="country" label="País" list={countries} />
+            </FormGroup>
+          </div>
+        </div>
+      </div>
+
+      {button && <div className="row align-items-center">
+        <div className="col-md-12 text-right">
+          <Button type="submit" disabled={isSubmitting} loading={isSubmitting}>
+            Cambiar datos
+          </Button>
+        </div>
+      </div> }
+    </Form>
+  )
+}
+
+// create Yup validation Schema
+const yupSchema = (countries, genders) => {
+  const nullable3CharMinString = Yup.string()
+    .trim().nullable()
+    .required('Obligatorio')
+    .min(3, 'Debe tener 3 caracteres o más.')
+
+  return Yup.object({
+    name: nullable3CharMinString
+      .max(25, 'Debe tener 25 caracteres o menos'),
+    document: nullable3CharMinString
+      .max(20, 'Debe tener 20 caracteres o menos'),
+    address: nullable3CharMinString
+      .max(80, 'Debe tener 80 caracteres o menos'),
+    city: nullable3CharMinString
+      .max(20, 'Debe tener 20 caracteres o menos'),
+    country: Yup.number().nullable()
+      .oneOf(countries.map(c => c.id), "País inválido")
+      .required('Obligatorio'),
+    gender: Yup.number().nullable()
+      .oneOf(genders.map(g => g.id), "Género inválido")
+      .required('Obligatorio'),
+  })
+}
+
+export default UserForm
--- pages/user/userData.js
+++ pages/user/userData.js
@@ -1,37 +1,21 @@
+// next
 import Router from 'next/router'
 import Head   from 'next/head'
-// import Link   from 'next/link'
-import { useEffect, useState }  from 'react'
-import { Formik, Form }         from 'formik'
-import * as Yup from 'yup'
+
+// react
 import nookies  from 'nookies'
-import { FkSelect, FkInput } from '~/components/Formik/fields'
-import FormGroup    from '~/components/layout/AuthModal/FormGroup'
-import withAuth     from '~/components/withAuth'
+
+//components
 import Layout       from '~/components/layout/Layout'
-import Button       from '~/components/button'
+import UserForm     from '~/components/form-user'
+import withAuth     from '~/components/withAuth'
 import api          from '~/services/api'
 import { CONFIG }   from '~/config'
 
 const userDataPage = ({ layoutProps, user, updateUser }) => {
-  const [ genders, setGenders ] = useState([])
-  const [ countries, setCountries ] = useState([])
 
-  /* get genders */
-  useEffect(_ => {
-    (async _ => {
-      const {data} = await api().get('genders')
-      setGenders(data)
-    })()
-  }, [])
-
-  /* get countries */
-  useEffect(_ => {
-    (async _ => {
-      const {data} = await api().get('countries')
-      setCountries(data)
-    })()
-  }, [])
+  const initialValues = (({gender_id: gender, country_id: country, ...data}) => {
+    return {gender, country, ...data}})(user)
 
   const handleSubmit = async  (fields, actions) => {
     var msg = ''
@@ -61,20 +45,9 @@ const userDataPage = ({ layoutProps, user, updateUser }) => {
           <div className="col-xl-8 offset-xl-2">
             <h1 className="h2">Datos Personales</h1>
             <hr />
-
-            { user &&
-              <Formik
-                initialValues={
-                  (({gender_id: gender, country_id: country, ...data}) => {
-                    return {gender, country, ...data}})(user)
-                }
-                validationSchema={
-                  getYupSchema(countries, genders)
-                }
-                onSubmit={handleSubmit}
-                component={props => <DataForm {...{countries, genders, ...props}} />}
-              />
-            }
+              {user &&
+                <UserForm {...{handleSubmit, initialValues}} button />
+              }
           </div>
         </div>
       </div>
@@ -99,80 +72,6 @@ const userDataPage = ({ layoutProps, user, updateUser }) => {
       `}</style>
     </Layout>
   );
-}
-
-const DataForm = ({isSubmitting, countries, genders})  => {
-
-  const cityLabel = CONFIG.lang === 'es-CL' ? 'Provincia' : 'Ciudad'
-
-  return (
-    <Form>
-      <div className="row">
-        <div className="col-md-6">
-          <div className="data">
-            <h4>Tus datos</h4>
-            <FormGroup>
-              <FkInput name="name" label="Nombre Completo" />
-            </FormGroup>
-            <FormGroup>
-              <FkSelect name="gender" label="Género" list={genders} />
-            </FormGroup>
-            <FormGroup>
-              <FkInput name="document" label="Documento" />
-            </FormGroup>
-          </div>
-        </div>
-
-        <div className="col-md-6">
-          <div className="localization">
-            <h3 className="h3">Ubicación</h3>
-            <FormGroup>
-              <FkInput name="address" label="Dirección" />
-            </FormGroup>
-            <FormGroup>
-              <FkInput name="city" label={cityLabel} />
-            </FormGroup>
-            <FormGroup>
-              <FkSelect name="country" label="País" list={countries} />
-            </FormGroup>
-          </div>
-        </div>
-      </div>
-
-      <div className="row align-items-center">
-        <div className="col-md-12 text-right">
-          <Button type="submit" disabled={isSubmitting} loading={isSubmitting}>
-            Cambiar datos
-          </Button>
-        </div>
-      </div>
-    </Form>
-  )
-}
-
-// create Yup validation Schema
-const getYupSchema = (countries, genders) => {
-  const nullable3CharMinString = Yup.string()
-    .trim().nullable()
-    .required('Obligatorio')
-    .min(3, 'Debe tener 3 caracteres o más.')
-
-  return Yup.object({
-    name: nullable3CharMinString
-      .max(25, 'Debe tener 25 caracteres o menos'),
-    document: nullable3CharMinString
-      .max(20, 'Debe tener 20 caracteres o menos'),
-    address: nullable3CharMinString
-      .max(80, 'Debe tener 80 caracteres o menos'),
-    city: nullable3CharMinString
-      .max(20, 'Debe tener 20 caracteres o menos'),
-    country: Yup.number().nullable()
-      .oneOf(countries.map(c => c.id), "País inválido")
-      .required('Obligatorio'),
-    gender: Yup.number().nullable()
-      .oneOf(genders.map(g => g.id), "Género inválido")
-      .required('Obligatorio'),
-  })
 }
 
 export default withAuth(userDataPage)
