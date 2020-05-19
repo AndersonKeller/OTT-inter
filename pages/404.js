bec8f661045dfedea450c89a03082666a37f1276
Giovanni Pires da Silva <giovanni@somosgad.com>
2020-05-19 09:35:13 -0300

    Separate 404 page due Next update warning

---
+++ pages/404.js
@@ -0,0 +1,39 @@
+import Error from 'next/error'
+import { STATIC_PATH } from '../constants/constants'
+import Color from 'color'
+import { useContext } from 'react'
+import { ThemeContext } from 'styled-components'
+
+export default function Custom404Page() {
+  const theme = useContext(ThemeContext)
+  const backgroundColor = Color(theme.colors.background).hsl().string()
+  const lightColor = Color(theme.colors.texts).hsl().string()
+  const whiteColor = Color(theme.colors.white).hsl().string()
+  return (
+    <div className="error">
+      <Error
+        statusCode={404}
+        title='No se pudo encontrar esta página'
+      />
+      <style global jsx>{`
+        .error > div {
+          background: ${backgroundColor} !important;
+          color: ${whiteColor} !important;
+        }
+        .error > div > div::after {
+          content: url(${STATIC_PATH}/logos/app.svg);
+          display: block;
+          height: auto;
+          margin-right: auto;
+          margin-top: 15px;
+          margin-left: auto;
+          max-width: 100%;
+          width: 150px;
+        }
+        .error > div h1 {
+          border-right-color: ${lightColor} !important;
+        }
+      `}</style>
+    </div>
+  )
+}
--- pages/_error.js
+++ pages/_error.js
@@ -8,13 +8,12 @@ import { ThemeContext } from 'styled-components'
 function CustomError({ statusCode }) {
   const theme = useContext(ThemeContext)
   const backgroundColor = Color(theme.colors.background).hsl().string()
+  const title = statusCode === 503 ? 'Servidor no disponible' : null
   return (
     <div className="error">
       <Error
         statusCode={statusCode}
-        title={ statusCode === 404 ? 'No se pudo encontrar esta página' :
-          statusCode === 503 ? 'Servidor no disponible' :
-          null }
+        title={title}
       />
       <style global jsx>{`
         .error > div {
@@ -46,24 +45,15 @@ CustomError.getInitialProps = async ({ res, err, asPath }) => {
   // getInitialProps has run
   errorInitialProps.hasGetInitialPropsRun = true
 
-
   if (res) {
     // Running on the server, the response object is available.
     //
     // Next.js will pass an err on the server if a page's `getInitialProps`
     // threw or returned a Promise that rejected
 
-    if (res.statusCode === 404) {
-      // Opinionated: do not record an exception in Sentry for 404
-      return { statusCode: 404 }
-    }
+    Sentry.captureException(err)
 
-    if (err) {
-      Sentry.captureException(err)
-
-      return errorInitialProps
-    }
-  } else {
+    return errorInitialProps
     // Running on the client (browser).
     //
     // Next.js will provide an err if:
@@ -73,11 +63,6 @@ CustomError.getInitialProps = async ({ res, err, asPath }) => {
     //    componentDidMount, etc) that was caught by Next.js's React Error
     //    Boundary. Read more about what types of exceptions are caught by Error
     //    Boundaries: https://reactjs.org/docs/error-boundaries.html
-    if (err) {
-      Sentry.captureException(err)
-
-      return errorInitialProps
-    }
   }
 
   // If this point is reached, getInitialProps was called without any
