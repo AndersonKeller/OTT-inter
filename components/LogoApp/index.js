5ffd5b59389c601d8d0c0377c31675717da2462a
Aline Prado <aline@dowhile.com.br>
2020-08-07 17:13:37 -0300

    Logos

---
+++ components/LogoApp/index.js
@@ -0,0 +1,30 @@
+import react, { useState, useContext, useEffect } from "react";
+import api from '~/services/api'
+import SearchContext from '~/contexts/SearchContext'
+
+
+function LogoApp() {
+  const { search, setSearch } = useContext(SearchContext);
+  const [link, setLink] = useState('https://laumas.s3.sa-east-1.amazonaws.com');
+  const [images, setImage] = useState([{ id: '1' }]);
+
+  useEffect(async () => {
+    const res = await api().get(search ? `/search/${search}` : '/club/images')
+    setImage(res.data);
+  }, [])
+  return (
+
+    <>
+      <img className="img-fluid" src={` ${link}/images/club/${images.image_projeto}`} />
+
+      <style jsx>{`
+        img {
+          display: inline-block;
+        }
+      `}</style>
+
+    </>
+
+  )
+}
+export default LogoApp;
---
+++ components/LogoClub/index.js
@@ -0,0 +1,42 @@
+import react, { useState, useContext, useEffect } from "react";
+import api from '~/services/api'
+import SearchContext from '~/contexts/SearchContext'
+
+
+function LogoApp() {
+  const { search, setSearch } = useContext(SearchContext);
+  const [link, setLink] = useState('https://laumas.s3.sa-east-1.amazonaws.com');
+  const [images, setImage] = useState([{ id: '1' }]);
+
+  useEffect(async () => {
+    const res = await api().get(search ? `/search/${search}` : '/club/images')
+    setImage(res.data);
+  }, [])
+  return (
+
+    <>
+      <img className="img-fluid" src={` ${link}/images/club/${images.image_logo}`} />
+
+      <style jsx>{`
+         img {
+         display: inline - block;
+        }
+        .logo {
+          align-items: center;
+          display: flex;
+          justify-content: center;
+          margin-top: -5px;
+          margin-bottom: -5px;
+        }
+        .logo a {
+          display: inline-flex;
+          padding: 5px;
+        }
+
+      `}</style>
+
+    </>
+
+  )
+}
+export default LogoApp;
