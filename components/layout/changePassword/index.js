35544fe1d1f8d45f5422cc099dd69d3f55988482
Aline Prado <aline@dowhile.com.br>
2020-09-11 13:49:01 -0300

    form-change-password

---
+++ components/layout/changePassword/index.js
@@ -0,0 +1,97 @@
+
+
+
+import styled from 'styled-components'
+export const StyleChangePassword = styled.div`
+
+
+    padding:20px;
+
+     label {
+       color: var(--gray);
+     }
+     .row{
+       width:100%;
+
+     }
+
+     .aling-button {
+       padding:  20px 0px 5px 0px;
+     }
+
+     .form-group {
+       width:100%;
+     }
+
+     .btn {
+      background-color: rgb(0, 142, 56)!important;
+      height: 34px;
+      padding: 0px;
+      font-size: 15px;
+      width: 150px;
+    }
+
+    .row-aling {
+       display: flex;
+       flex-wrap: inherit;
+    }
+
+
+    .row-aling {
+      justify-content:  space-between;
+    }
+
+
+     h1 {
+        display: flex;
+        justify-content: center;
+        color: black;
+        padding:10px;
+     }
+
+     .painel {
+        width: 58%;
+     }
+
+    @media(max-width:765px) {
+
+      .painel {
+       width: 100%;
+       justify-content: center;
+       display: contant;
+      }
+
+      .row-aling {
+      justify-content:center
+      }
+      .input-aling {
+         width: 88%;
+
+      }
+      .row {
+            display: contents;
+      }
+
+      .btn {
+        padding: 7px;
+
+      }
+
+      .aling-button {
+        display: flex;
+       justify-content: center;
+      }
+
+
+    }
+
+
+    }
+
+
+
+`
+
+
+
+
--- pages/user/changePassword.js
+++ pages/user/changePassword.js
@@ -1,44 +1,46 @@
 // nextjs imports
 import Router from 'next/router'
-import Head   from 'next/head'
+import Head from 'next/head'
 // import Link   from 'next/link'
 
 // react imports
 import { Formik, Form } from 'formik'
 import * as Yup from 'yup'
-import nookies  from 'nookies'
+import nookies from 'nookies'
 
 // app imports
-import FormGroup    from '~/components/layout/AuthModal/FormGroup'
-import withAuth     from '~/components/withAuth'
-import { FkInput }  from '~/components/Formik/fields'
-import Layout       from '~/components/layout/Layout'
-import Button       from '~/components/button'
-import api          from '~/services/api'
-import { CONFIG }   from '~/config'
+import FormGroup from '~/components/layout/AuthModal/FormGroup'
+import withAuth from '~/components/withAuth'
+import { FkInput } from '~/components/Formik/fields'
+import Layout from '~/components/layout/Layout'
+import Button from '~/components/button'
+import api from '~/services/api'
+import { CONFIG } from '~/config'
+import { StyleChangePassword } from '~/components/layout/changePassword/index'
+import CardLogoHeader from '~/components/CardLogoHeader/index'
 
 const passwordPage = ({ layoutProps, user }) => {
 
-  const handleSubmit = async  (fields, actions) => {
+  const handleSubmit = async (fields, actions) => {
     var msg = undefined
 
     try {
-      const { data:{ message } } = await api().post('password', {
+      const { data: { message } } = await api().post('password', {
         email: fields.username,
         ...fields
       })
       msg = { success: message || "Cambio de datos completo." }
 
-    }catch({ response:{ data:{ errors } }, message: error }) {
+    } catch ({ response: { data: { errors } }, message: error }) {
       msg = errors ?
         actions.setErrors(errors) : { error: 'Problemas al realizar esta operación. Inténtalo de nuevo más tarde' }
 
       console.log(error)
 
-    }finally {
-      if(msg) {
+    } finally {
+      if (msg) {
         msg = JSON.stringify(msg)
-        nookies.set({}, 'flash_message', msg , { path: '/' })
+        nookies.set({}, 'flash_message', msg, { path: '/' })
         Router.push('/user/account')
       }
     }
@@ -49,26 +51,19 @@ const passwordPage = ({ layoutProps, user }) => {
       <Head>
         <title>Cambiar la Contraseña &lt; {CONFIG.appName}</title>
       </Head>
-      <div className="rgpage container-fluid">
-        <div className="row">
-          <div className="col-md-8 offset-md-2">
-            <h1 className="h2">Cambiar la Contraseña</h1>
-            { user &&
-              <Formik
-                initialValues={
-                  (({ email: username }) => {
-                    return { username, oldpassword: '', password: '', password_confirmation: '' }
-                  })(user)
-                }
-                validationSchema={ getYupSchema() }
-                onSubmit={handleSubmit}
-                component={DataForm}
-              />
-            }
-          </div>
-        </div>
-      </div>
-      <style jsx>{`
+      {user &&
+        <Formik
+          initialValues={
+            (({ email: username }) => {
+              return { username, oldpassword: '', password: '', password_confirmation: '' }
+            })(user)
+          }
+          validationSchema={getYupSchema()}
+          onSubmit={handleSubmit}
+          component={DataForm}
+        />
+      }
+      {/* <style jsx>{`
         .rgpage {
           padding-top: 40px;
           padding-bottom: 120px;
@@ -81,40 +76,56 @@ const passwordPage = ({ layoutProps, user }) => {
           margin-bottom: 15px;
           background-color: white;
         }
-      `}</style>
+      `}</style> */}
     </Layout>
   );
 
 }
 
 const DataForm = ({ isSubmitting, status }) => <Form>
-  { status && <div className="invalid-feedback"><h5>{status.error}</h5></div> }
-  <FormGroup>
-    <FkInput name="username" type="hidden"
-      label="Username" autoComplete="username" />
-  </FormGroup>
-  <FormGroup>
-    <FkInput name="oldpassword" type="password"
-      label="Contraseña anterior" autoComplete="current-password" />
-  </FormGroup>
-  <FormGroup>
-    <FkInput name="password" type="password"
-      label="Nueva contraseña" autoComplete="new-password" />
-  </FormGroup>
-  <FormGroup>
-    <FkInput name="password_confirmation" type="password"
-      label="Confirmar nueva contraseña" autoComplete="new-password" />
-  </FormGroup>
-  <hr />
+  {status && <div className="invalid-feedback"><h5>{status.error}</h5></div>}
+  <CardLogoHeader>
+    <StyleChangePassword>
+      <h1 className="h2">Cambiar la contraseña</h1>
 
-  <div className="row">
-    <div className="col-md-12 text-right">
-      <Button type="submit" disabled={isSubmitting} loading={isSubmitting}>
-        Cambiar datos
+      <div class="painel">
+        <div className="row" >
+          <FormGroup>
+            <FkInput name="username" type="hidden"
+              label="Username" autoComplete="username" />
+          </FormGroup>
+        </div>
+        <div className="row" >
+          <FormGroup>
+            <FkInput name="oldpassword" type="password"
+              label="Contraseña anterior" autoComplete="current-password" />
+          </FormGroup>
+        </div>
+        <div className="row" >
+          <FormGroup>
+            <FkInput name="password" type="password"
+              label="Nueva contraseña" autoComplete="new-password" />
+          </FormGroup>
+        </div>
+      </div>
+      <div className="row row-aling">
+        <div className="painel">
+          <FormGroup >
+            <FkInput name="password_confirmation" type="password"
+              label="Confirmar nueva contraseña" autoComplete="new-password" />
+          </FormGroup>
+          <hr />
+        </div>
+        <div className=" aling-button">
+          <Button type="submit" disabled={isSubmitting} loading={isSubmitting}>
+            Aceptar
       </Button>
-    </div>
-  </div>
-</Form>
+        </div>
+      </div>
+
+    </StyleChangePassword>
+  </CardLogoHeader >
+</Form >
 
 // create Yup validation Schema
 const getYupSchema = _ => {
