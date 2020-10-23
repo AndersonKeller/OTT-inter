1edc5d4439ec606ce9c56d592331f9c9b7dbac4e
Aline Prado <aline@dowhile.com.br>
2020-10-23 16:21:52 -0300

    Ajuste-favicon

--- pages/index.js
+++ pages/index.js
@@ -23,10 +23,10 @@ const HomePage = ({ api, contents, featuredMedia, featuredMediaError, media, lay
   return (
     <Layout paddingTop={false} {...layoutProps} media={featuredMedia}>
       <Head>
-        <title>{pageTitle}</title>
-        <link rel="apple-touch-icon" sizes="180x180" href={`/static/${TENANT}/favicon/apple-touch-icon.png`} />
-        <link rel="icon" type="image/png" sizes="32x32" href={`/static/${TENANT}/favicon/favicon-32x32.png`} />
-        <link rel="icon" type="image/png" sizes="16x16" href={`/static/${TENANT}/favicon/favicon-16x16.png`} />
+        <title>{pageTitle}</title>x
+        <link rel="apple-touch-icon" sizes="180x180" href={`/static/${TENANT}/favicon/faviconlau-180x180.png`} />
+        <link rel="icon" type="image/png" sizes="32x32" href={`/static/${TENANT}/favicon/faviconlau-32x32.png`} />
+        <link rel="icon" type="image/png" sizes="16x16" href={`/static/${TENANT}/favicon/faviconlau-16x16.png`} />
         <link rel="manifest" href={`/static/${TENANT}/favicon/site.webmanifest`} />
         <link rel="mask-icon" href={`/static/${TENANT}/favicon/safari-pinned-tab.svg`} color="#5bbad5" />
         <meta name="msapplication-TileColor" content="#da532c" />
