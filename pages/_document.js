5c80c017d9bfac22168048becb7640cf348a4f28
Rafael Fonseca <rafael@dowhile.com.br>
2020-12-07 17:03:41 -0300

    Fix analytics and add new script

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,4 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Head, Html, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 import { IS_PRODUCTION } from '~/constants/constants'
 import theme from '~/theme'
@@ -6,11 +6,11 @@ import * as Sentry from '@sentry/browser'
 import { GA_TRACKING_ID } from '~/lib/gtag'
 
 process.on('unhandledRejection', (err) => {
-    Sentry.captureException(err);
+  Sentry.captureException(err);
 });
 
 process.on('uncaughtException', (err) => {
-    Sentry.captureException(err);
+  Sentry.captureException(err);
 });
 
 export default class MyDocument extends Document {
@@ -22,7 +22,7 @@ export default class MyDocument extends Document {
     try {
       ctx.renderPage = () =>
         originalRenderPage({
-          enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
+          enhanceApp: App => props => sheet.collectStyles(<App { ...props } />),
         })
 
       const initialProps = await Document.getInitialProps(ctx)
@@ -30,8 +30,8 @@ export default class MyDocument extends Document {
         ...initialProps,
         styles: (
           <>
-            {initialProps.styles}
-            {sheet.getStyleElement()}
+            { initialProps.styles }
+            { sheet.getStyleElement() }
           </>
         ),
       }
@@ -50,20 +50,31 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
 
-          <meta charSet="utf-8" />
-          <meta name="viewport" content="initial-scale=1, shrink-to-fit=yes, width=device-width, viewport-fit=cover" />
+          <script
+            dangerouslySetInnerHTML={ {
+              __html: ` (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-W62Q65V');
+          `
+            } }></script>
 
-          {/* PWA primary color */}
-          <meta name="theme-color" content={theme.colors.primary} />
+          <meta charSet="utf-8"/>
+          <meta name="viewport" content="initial-scale=1, shrink-to-fit=yes, width=device-width, viewport-fit=cover"/>
 
-          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          {/* PWA primary color */ }
+          <meta name="theme-color" content={ theme.colors.primary }/>
+
+          {/* Global Site Tag (gtag.js) - Google Analytics */ }
           <script
             async
-            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+            src={ `https://www.googletagmanager.com/gtag/js?id=${ GA_TRACKING_ID }` }
           />
 
           <script
-          dangerouslySetInnerHTML={{__html: ` !function(f,b,e,v,n,t,s)
+            dangerouslySetInnerHTML={ {
+              __html: ` !function(f,b,e,v,n,t,s)
           {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
           n.callMethod.apply(n,arguments):n.queue.push(arguments)};
           if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
@@ -73,35 +84,58 @@ export default class MyDocument extends Document {
           'https://connect.facebook.net/en_US/fbevents.js');
           fbq('init', '397493668062436');
           fbq('track', 'PageView');
-          `}}></script>
+          `
+            } }></script>
 
-          <noscript dangerouslySetInnerHTML={{ __html: `<img height="1" width="1" style="display:none"
-          src="https://www.facebook.com/tr?id=397493668062436&ev=PageView&noscript=1" />` }}
+          <script
+            dangerouslySetInnerHTML={ {
+              __html: `
+              (function() {
+                window.__Insp = window.__Insp || [];
+                __insp.Push(['wid', 1793852315]);
+                var ldinsp = function() {
+                    if (typeof window.__inspld! = "undefined") return;
+                    window.__inspld = 1;
+                    var insp = document.createElement('script');
+                    insp.type = 'text / javascript';
+                    insp.assíncrono = verdadeiro;
+                    insp.id = "inspsync";
+                    insp.src = ('https:' == documento.localização.protocolo ? 'https' : 'http') + ': //cdn.inspectlet.com/inspectlet.js? wid = 1793852315 & r =' + Math.floor(new Date().getTime() / 3600000);
+                    var x = documento.getElementsByTagName('script')[0];
+                    x.parentNode.insertBefore(insp, x);
+                };
+                setTimeout(ldinsp, 0);
+          })();`}}></script>
+
+          <noscript dangerouslySetInnerHTML={ {
+            __html: `<img height="1" width="1" style="display:none"
+          src="https://www.facebook.com/tr?id=397493668062436&ev=PageView&noscript=1" />`
+          } }
           />
 
           <script
-            dangerouslySetInnerHTML={{
+            dangerouslySetInnerHTML={ {
               __html: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', '${GA_TRACKING_ID}', {
+            gtag('config', '${ GA_TRACKING_ID }', {
               page_path: window.location.pathname,
             });
           `,
-            }}
+            } }
           />
 
-          {/* styles */}
+          {/* styles */ }
           { IS_PRODUCTION ? <>
-            {/* bootstrap */}
+            {/* bootstrap */ }
             <link
               crossOrigin="anonymous"
               href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
               integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
               rel="stylesheet"
-              />
-            {/* slick */}
+            />
+            {/* slick */ }
             <link
               charSet="utf-8"
               href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
@@ -113,28 +147,34 @@ export default class MyDocument extends Document {
               rel="stylesheet"
               type="text/css"
             />
-            {/* roboto & roboto condensed */}
+            {/* roboto & roboto condensed */ }
             <link
               href="https://fonts.googleapis.com/css?family=Roboto:400,700|Roboto+Condensed:700&amp;display=swap"
               rel="stylesheet"
             />
           </> : <>
-            {/* bootstrap */}
-            <link rel="stylesheet" href="/static/css/bootstrap.min.css" />
-            {/* slick */}
-            <link rel="stylesheet" href="/static/css/slick.css" />
-            <link rel="stylesheet" href="/static/css/slick-theme.css" />
-            {/* roboto & roboto condensed */}
-            <link rel="stylesheet" href="/static/fonts/roboto/stylesheet.css" />
+            {/* bootstrap */ }
+            <link rel="stylesheet" href="/static/css/bootstrap.min.css"/>
+            {/* slick */ }
+            <link rel="stylesheet" href="/static/css/slick.css"/>
+            <link rel="stylesheet" href="/static/css/slick-theme.css"/>
+            {/* roboto & roboto condensed */ }
+            <link rel="stylesheet" href="/static/fonts/roboto/stylesheet.css"/>
           </> }
 
-          {/* bebas */}
-          <link rel="stylesheet" href="/static/fonts/bebas-neue/stylesheet.css" />
+          {/* bebas */ }
+          <link rel="stylesheet" href="/static/fonts/bebas-neue/stylesheet.css"/>
 
         </Head>
         <body>
-          <Main />
-          <NextScript />
+        <noscript dangerouslySetInnerHTML={{
+          __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W62Q65V"
+                  height="0" width="0" style="display:none;visibility:hidden"></iframe>`
+        }}
+        />
+
+        <Main/>
+        <NextScript/>
         </body>
       </Html>
     )
