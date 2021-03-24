949bb6e49689f6b78650baff78a6d168c0cb5cea
Aline Prado <aline_100lika@hotmail.com>
2021-03-24 15:41:52 -0300

    ajust-chave

--- pages/user/changePlan/pay.js
+++ pages/user/changePlan/pay.js
@@ -53,8 +53,8 @@ const pay = withRouter(({ packages, handleSubmit, }) => {
   const primaryColor = Color(theme.colors.primary)
   const [cardImg, setCardImg] = useState(null);
   const MercadoPago = ready && HAS_WINDOW ? window.Mercadopago : null
-  // const businessUnitPublicKey = 'APP_USR-fe20d55f-f7d1-49e0-855b-4d5c147ddd0b'
-  const businessUnitPublicKey = "TEST-5121749c-2a58-4b7d-b98c-9b9932a3a4cc";
+  const businessUnitPublicKey = 'APP_USR-fe20d55f-f7d1-49e0-855b-4d5c147ddd0b'
+  // const businessUnitPublicKey = "TEST-5121749c-2a58-4b7d-b98c-9b9932a3a4cc";
 
 
   const [isMercadoPagoReady, setIsMercadoPagoReady] = useState(false)
